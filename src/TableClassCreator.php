473f4a4bab7ce538bdb1ffef0a431ddcaf920c02
Programster <sdpagent@gmail.com>
2021-07-06 15:27:32 +0100

    updates table class creator to create table objects that extend "AbstractTable".

--- src/TableClassCreator.php
+++ src/TableClassCreator.php
@@ -7,53 +7,48 @@ class TableClassCreator extends AbstractView
     private $m_tableName;
     private $m_tableClassName;
     private $m_objectClassName;
-    
-    
+
+
     public function __construct(string $tableClassName, string $objectClassName, string $tableName)
     {
         $this->m_tableName = $tableName;
         $this->m_objectClassName = $objectClassName;
         $this->m_tableClassName = $tableClassName;
     }
-    
-    
-    protected function renderContent() 
+
+
+    protected function renderContent()
     {
         print '<?php' . PHP_EOL . PHP_EOL;
 ?>
 
 
-class <?= $this->m_tableClassName; ?> extends AbstractVidaTable
-{    
-    protected function __construct() 
+class <?= $this->m_tableClassName; ?> extends AbstractTable
+{
+    public function getObjectClassName()
     {
+        return __NAMESPACE__ . '\<?= $this->m_objectClassName; ?>';
     }
-    
-    
-    public function getObjectClassName() 
-    { 
-        return __NAMESPACE__ . '\<?= $this->m_objectClassName; ?>'; 
+
+
+    public function getTableName()
+    {
+        return '<?= $this->m_tableName ?>';
     }
-    
-    
-    public function getTableName() 
-    { 
-        return '<?= $this->m_tableName ?>'; 
+
+
+    public function validateInputs(array $data)
+    {
+        return $data;
     }
-    
-    
-    public function validateInputs(array $data) 
-    { 
-        return $data; 
+
+
+    public function getFieldsThatAllowNull()
+    {
+        return array();
     }
-    
-    
-    public function getFieldsThatAllowNull() 
-    { 
-        return array(); 
-    }
-    
-    
+
+
     public function getFieldsThatHaveDefaults()
     {
         return array();
